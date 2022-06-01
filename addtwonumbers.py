55adf4aac7472e202604b487f2d0c45efa5a6efe
Steina25 <steina25@greenhill.org>
2022-06-01 12:37:06 -0500

    June 1 commit

---
+++ addtwonumbers.py
@@ -0,0 +1,10 @@
+# Aaron Stein
+# add two numbers together
+import os
+os.system('cls')
+number1 = 23
+number2 = 16
+add= number 1 + number 2
+print(add)
+print(number1 + number2)
+
---
+++ bmi.py
@@ -0,0 +1,13 @@
+#Aaron Stein
+#find bmi
+import os
+os.ststem('cls')
+number1 = int(input(55))
+number2 = int(input(2))
+m2 = number1 * number1
+bmi = m2 / number2
+print(bmi)
+if(bmi < 14.50):
+print("your underwieght")
+if(bmi > 25):
+print("your overwieght")
