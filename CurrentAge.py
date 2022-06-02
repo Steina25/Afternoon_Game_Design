db751b47d349d895cc78fcdd8a8504ea61ca578a
Steina25 <steina25@greenhill.org>
2022-06-01 22:04:06 -0500

    Homework 2 commit

---
+++ CurrentAge.py
@@ -0,0 +1,11 @@
+# Aaron Stein
+# Current Age
+
+import os
+os.system('cls')
+number1 = int(input(2022))
+number2 = int(input(2006))
+Current Age = number1 - number2
+print (Current Age)
+if(Current Age < 12):
+    print ("teenager")
